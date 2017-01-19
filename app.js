ecaeed86c8e52f99772e77318343b0038755132f
Moderntotem4 <moderntotem.yt@gmail.com>
2017-01-18 20:13:59 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var onlyButton = document.querySelector('.clickhere');
+onlyButton.addEventListener('click', function(){
+    document.getElementById('ept').classList.add('addedClass');
+});
+
+
+
+
+    document.getElementById('ept').innerHTML = "I was just added";
